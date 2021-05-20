628c2a578796958954cd60df0c24b9736f0b1294
daniel-evans007 <mr.anirbangoswami@gmail.com>
2021-05-20 15:12:22 +0530

    modified some files

--- Codes/Anirban-LabBook/src/com/cg/eis/service/EmployeeInsuranceSystem.java
+++ Codes/Anirban-LabBook/src/com/cg/eis/service/EmployeeInsuranceSystem.java
@@ -31,17 +31,17 @@ public class EmployeeInsuranceSystem implements EmployeeService{
 		
 		emp.setInsuranceScheme("No Scheme");
 		
-		if (emp.getSalary() < 5000 && emp.getDesignation().equalsIgnoreCase("Daily Wage Worker")) {
+		if (emp.getSalary() < 5000 && emp.getDesignation().equalsIgnoreCase("Clerk")) {
 			emp.setInsuranceScheme("No Scheme");
 		}
-		else if (emp.getSalary() >= 5000 && emp.getSalary() < 20000 && emp.getDesignation().equalsIgnoreCase("Clerk")) {
-			emp.setInsuranceScheme("Scheme #1");
+		else if (emp.getSalary() > 5000 && emp.getSalary() < 20000 && emp.getDesignation().equalsIgnoreCase("System Associate")) {
+			emp.setInsuranceScheme("Scheme C");
 		}
-		else if (emp.getSalary() >= 40000 && emp.getSalary() < 40000 && emp.getDesignation().equalsIgnoreCase("Analyst")) {
-			emp.setInsuranceScheme("Scheme #2");
+		else if (emp.getSalary() >= 20000 && emp.getSalary() < 40000 && emp.getDesignation().equalsIgnoreCase("Programmer")) {
+			emp.setInsuranceScheme("Scheme B");
 		}
-		else if (emp.getSalary() >= 40000 && emp.getSalary() < 70000 && emp.getDesignation().equalsIgnoreCase("Manager")) {
-			emp.setInsuranceScheme("Scheme #3");
+		else if (emp.getSalary() >= 40000 && emp.getDesignation().equalsIgnoreCase("Manager")) {
+			emp.setInsuranceScheme("Scheme A");
 		}
 	}
 	
