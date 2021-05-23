c16dd0f4a4b3f168b22990e81b85bc9764e5db10
daniel-evans007 <mr.anirbangoswami@gmail.com>
2021-05-24 00:03:54 +0530

    added code for exception handling exercise 1

---
+++ Codes/Anirban-LabBook/src/com/cg/lab_five/Exercise1.java
@@ -0,0 +1,39 @@
+package com.cg.lab_five;
+
+import java.util.Scanner;
+
+@SuppressWarnings("serial")
+class AgeException extends Exception {
+
+	public AgeException(String s) {
+		super(s);
+	}
+}
+
+public class Exercise1 {
+	
+	public static void validateAge(int age) throws AgeException {
+		if (age <= 15) {
+			throw new AgeException("Age must be above 15 years");
+		}
+		else {
+			System.out.println("Age is perfectly valid");
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			System.out.print("Enter age: ");
+			int age = sc.nextInt();
+			validateAge(age);
+		}
+		catch (AgeException e) {
+			System.out.print("Error message: "+e.getMessage());
+		}
+		
+		sc.close();
+	}
+}
