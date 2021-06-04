ee0bd756de36b455787050e4a31fea34c3976dd8
daniel-evans007 <mr.anirbangoswami@gmail.com>
2021-06-04 12:11:50 +0530

    added some files

---
+++ Codes/Java/src/com/cg/day14/predicateinarraylist/FunctionalInterfaceInStudent.java
@@ -0,0 +1,40 @@
+package com.cg.day14.predicateinarraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionalInterfaceInStudent {
+
+	public static void main(String[] args) {
+		
+		List<Student> studentList = new ArrayList<Student>();
+		
+		studentList.add(new Student(100, "Danny"));
+		studentList.add(new Student(101, "Kenny"));
+		studentList.add(new Student(102, "Ben"));
+		
+		Function<Student, String> funcStdToString = (Student s) -> {
+			return s.getStudentName();
+		};
+		
+		Function<String, String> subStr = (String s) -> s.substring(0,1);
+		
+		List<String> studentNameInitials = convertStudentNameListToNamesList(studentList,
+				funcStdToString.andThen(subStr));
+		
+		System.out.println("Student Name Initials...");
+		
+		studentNameInitials.forEach(str -> {System.out.println(str);});
+	}
+	
+	public static List<String> convertStudentNameListToNamesList(List<Student> studentList,
+			Function<Student, String>funcStdToString) {
+		List<String> studentNameList = new ArrayList<String>();
+		
+		for (Student s : studentList) {
+			studentNameList.add(funcStdToString.apply(s));
+		}
+		return studentNameList;
+	}
+}
