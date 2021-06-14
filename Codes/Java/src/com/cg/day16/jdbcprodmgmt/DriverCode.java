7fde111c04867ff7ee502e609b620c435d28eb41
daniel-evans007 <mr.anirbangoswami@gmail.com>
2021-06-14 16:15:22 +0530

    adding jdbc demoes

--- Codes/Java/src/com/cg/day16/jdbcprodmgmt/DriverCode.java
+++ Codes/Java/src/com/cg/day16/jdbcprodmgmt/DriverCode.java
@@ -1,13 +1,57 @@
 package com.cg.day16.jdbcprodmgmt;
 
-public class DriverCode {
+import java.sql.SQLException;
+import java.util.Scanner;
 
-	public static void main(String[] args) throws Exception {
+public class DriverCode {
+	
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		
 		ProductManagement pms = new ProductManagement();
+		Scanner sc = new Scanner(System.in);
 		pms.dbConnection();
-		//pms.createTableProduct();
-		//pms.insertIntoProduct();
-		pms.insertIntoProductByPS();
+ 
+		String cont = "yes";
+		while (cont.equalsIgnoreCase("yes")) {
+			System.out.println("insert : to insert product");
+			System.out.println("update : to update product");
+			System.out.println("delete : to delete product");
+			System.out.println("getproduct : to a get single product");
+			System.out.println("getallproducts : to a single get all products");
+			
+			System.out.print("\nEnter the choice : ");
+			String transaction = sc.nextLine();
+			
+			switch (transaction) {
+			case "insert":
+				pms.insertIntoProductByPS();
+				break;
+			case "update":
+				pms.updateProductUsingId();
+				break;
+			case "delete":
+				pms.deleteProductUsingId();
+				break;
+			case "getproduct":
+				pms.getProductUsingId();
+				break;
+			case "getallproducts":
+				pms.getAllProducts();
+				break;
+			default:
+				System.out.println("Invalid choice...");
+				break;
+			}
+			System.out.println();
+			System.out.println("do you want continue? yes or no");
+			System.out.print("Response: ");
+			cont = sc.nextLine();
+			
+			if (!cont.equalsIgnoreCase("yes")) {
+				System.out.println("Exiting program successfully...");
+				System.exit(0);
+			}
+			System.out.println();
+		}
 	}
 }
--- Codes/Java/src/com/cg/day16/jdbcprodmgmt/ProductManagement.java
+++ Codes/Java/src/com/cg/day16/jdbcprodmgmt/ProductManagement.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
@@ -15,13 +16,13 @@ public class ProductManagement {
 	static PreparedStatement pstmt = null;
 	static ResultSet rs = null;
 	
-	public Connection dbConnection() throws Exception {
+	public Connection dbConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "oracle");
 		return con;
 	}
 	
-	public void createTableProduct() throws Exception {
+	public void createTableProduct() throws SQLException {
 		stmt = con.createStatement();
 		String createTable = "create table "
 				+ "danny_dbms.products(pid number, pname varchar2(20), price number(10, 2))";
@@ -33,11 +34,10 @@ public class ProductManagement {
 		else {
 			System.out.println("\nTable not Created successfully");
 		}
-		
 		stmt.close();
 	}
 	
-	public void insertIntoProduct() throws Exception {
+	public void insertIntoProduct() throws SQLException {
 		stmt = con.createStatement();
 		String insert = "insert into danny_dbms.products values(126, 'Mouse', 500.0)";
 		int i = stmt.executeUpdate(insert);
@@ -48,12 +48,10 @@ public class ProductManagement {
 		else {
 			System.out.println("\nData could not be inserted");
 		}
-		
 		stmt.close();
 	}
 	
-	public void insertIntoProductByPS() throws Exception {
-		stmt = con.createStatement();
+	public void insertIntoProductByPS() throws SQLException {
 		String insert = "insert into danny_dbms.products values(?, ?, ?)";
 		pstmt = con.prepareStatement(insert);
 		readProductDetails();
@@ -65,12 +63,10 @@ public class ProductManagement {
 		else {
 			System.out.println("\nData could not be inserted");
 		}
-		
-		stmt.close();
-		sc.close();
+		pstmt.close();
 	}
 	
-	public static void readProductDetails() throws Exception {
+	public static void readProductDetails() throws SQLException {
 		System.out.println("\nEnter the product Details...\n");
 		System.out.print("Enter the product ID: ");
 		int pid = Integer.parseInt(sc.nextLine());
@@ -83,19 +79,76 @@ public class ProductManagement {
 		pstmt.setFloat(3, price);
 	}
 	
-	public void getAllProduct() {
+	public void getAllProducts() throws SQLException {
+		stmt =  con.createStatement();
+		rs = stmt.executeQuery("select * from danny_dbms.products");
+		System.out.println("PID\tPNAME\tPRICE");
 		
+		while (rs.next()) {
+			System.out.println(rs.getString(1)+"\t"+rs.getString(2)+"\t"+rs.getString(3));
+		}
+		stmt.close();
+		rs.close();
 	}
 	
-	public void getProductUsingId() {
+	public void getProductUsingId() throws SQLException {
+		String display = "select * from danny_dbms.products where pid = ?";
 		
+		System.out.print("Enter the Product ID: ");
+		int pid = Integer.parseInt(sc.nextLine());
+		pstmt.setInt(1, pid);
+		rs = pstmt.executeQuery(display);
+		
+		System.out.println();
+		System.out.println("PID\tPNAME\tPRICE");
+		
+		while (rs.next()) {
+			System.out.println(rs.getString(1)+"\t"+rs.getString(2)+"\t"+rs.getString(3));
+		}
+		pstmt.close();
+		rs.close();
 	}
 	
-	public void updateProductUsingId() {
-		
+	public void updateProductUsingId() throws SQLException {
+		String updateProduct = "update danny_dbms.products set pid = ?,pname=? where price = ?";
+		pstmt = con.prepareStatement(updateProduct);
+		System.out.print("Enter the Product ID: ");
+		int pid = Integer.parseInt(sc.nextLine());
+		System.out.print("Enter the Product Name: ");
+		String pname = sc.nextLine();
+		System.out.print("Enter the Product Price: ");
+		float price = Float.parseFloat(sc.nextLine());
+			
+		pstmt.setInt(1, pid);
+		pstmt.setString(2, pname);
+		pstmt.setFloat(3, price);
+			
+		int i = pstmt.executeUpdate();
+			
+		if(i != 0) {
+			System.out.println("Data updated successfully");
+		}
+		else {
+			System.out.println("Data not updated successfully");
+		}
+		pstmt.close();
 	}
 	
-	public void deleteProductUsingId() {
+	public void deleteProductUsingId() throws SQLException {
+		String delete = "delete from product where pid = ?";
+		pstmt = con.prepareStatement(delete);
+		System.out.print("Enter the Product ID: ");
+		int pid = Integer.parseInt(sc.nextLine());
+		pstmt.setInt(1, pid);
+		int i = pstmt.executeUpdate();
 		
+		if (i == 1) {
+			System.out.println("Product has been deleted successfully");
+		}
+		else {
+			System.out.println("Product hasn't been deleted");
+		}
+		pstmt.close();
+		rs.close();
 	}
 }
