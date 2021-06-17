086341defe5950389cd5a9facb989a6e82c1eb96
daniel-evans007 <mr.anirbangoswami@gmail.com>
2021-06-17 09:37:39 +0530

    hibernate one to one

---
+++ Codes/Spring/HibernateOneToOne/src/main/java/com/cg/hibernateonetoone/entity/Instructor.java
@@ -0,0 +1,76 @@
+package com.cg.hibernateonetoone.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Instructor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "instructor_detail_id")
+	private InstructorDetail instructorDetail;
+
+	public Instructor() {
+		super();
+	}
+
+	public Instructor(String firstName, String lastName, String email, InstructorDetail instructorDetail) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.instructorDetail = instructorDetail;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public InstructorDetail getInstructorDetail() {
+		return instructorDetail;
+	}
+
+	public void setInstructorDetail(InstructorDetail instructorDetail) {
+		this.instructorDetail = instructorDetail;
+	}
+
+	@Override
+	public String toString() {
+		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", instructorDetail=" + instructorDetail + "]";
+	}
+}
---
+++ Codes/Spring/HibernateOneToOne/src/main/java/com/cg/hibernateonetoone/entity/InstructorDetail.java
@@ -0,0 +1,52 @@
+package com.cg.hibernateonetoone.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class InstructorDetail {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String youtubechannel;
+	private String hobby;
+	
+	public InstructorDetail() {
+		super();
+	}
+
+	public InstructorDetail(String youtubechannel, String hobby) {
+		super();
+		this.youtubechannel = youtubechannel;
+		this.hobby = hobby;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getYoutubechannel() {
+		return youtubechannel;
+	}
+
+	public void setYoutubechannel(String youtubechannel) {
+		this.youtubechannel = youtubechannel;
+	}
+
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	@Override
+	public String toString() {
+		return "InstructorDetail [id=" + id + ", youtubechannel=" + youtubechannel + ", hobby=" + hobby + "]";
+	}
+}
